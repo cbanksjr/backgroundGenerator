034016ee770e83e6b65717c22f92d80980f9ac96
Craig Banks Jr <46584614+cbanksjr@users.noreply.github.com>
2022-12-23 20:33:54 -0500

    Add files via upload
    Moved buttonText variable to outside of the colorRandomizer function and put it on top with the other DOM methods.
    Created insertButton function to make cleaner code. Moved DOM methods from colorRandomizer into insertButton.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 var btn = document.createElement('button');
-
+var buttonText = document.createTextNode('Random Colors');
 
 
 function colorMatch() {
@@ -18,14 +18,15 @@ function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
     css.textContent = body.style.background + ";";
+
 }
 
-
-function colorRandomizer() {
-    var buttonText = document.createTextNode('Random Colors');
+function insertButton() {
     body.insertBefore(btn, body.children[3]);
     btn.appendChild(buttonText);
-    
+}
+
+function colorRandomizer() {
     btn.onclick = () => {
         var generateColor = Math.floor(Math.random() * 16777215).toString(16);
         var generateColor2 = Math.floor(Math.random() * 16777215).toString(16);
@@ -44,4 +45,8 @@ color2.addEventListener('input', setGradient);
 
 colorMatch();
 
+insertButton();
+
 colorRandomizer();
+
+
