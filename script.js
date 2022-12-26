fb8f512cea14f0380c46367ec29041985e8cc0d6
Craig Banks Jr <46584614+cbanksjr@users.noreply.github.com>
2022-12-26 11:36:28 -0500

    Add files via upload
    Updated syntax for functions

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var css = document.querySelector('h3');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
-var body = document.getElementById('gradient');
-var btn = document.createElement('button');
-var buttonText = document.createTextNode('Random Colors');
+let  css = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let  color2 = document.querySelector('.color2');
+let  body = document.getElementById('gradient');
+let  btn = document.createElement('button');
+let  buttonText = document.createTextNode('Random Colors');
 
 
-function colorMatch() {
+let colorMatch = () => {
   color1.setAttribute("value", "#ff0000" );
   color2.setAttribute("value", "#ffff00");
   setGradient();
@@ -14,19 +14,19 @@ function colorMatch() {
 }
 
 
-function setGradient() {
+let setGradient = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
     css.textContent = body.style.background + ";";
 
 }
 
-function insertButton() {
+let insertButton = () => {
     body.insertBefore(btn, body.children[3]);
     btn.appendChild(buttonText);
 }
 
-function colorRandomizer() {
+let colorRandomizer = () => {
     btn.onclick = () => {
         var generateColor = Math.floor(Math.random() * 16777215).toString(16);
         var generateColor2 = Math.floor(Math.random() * 16777215).toString(16);
